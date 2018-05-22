21a30a70bea4585383f5c12a993bbba094c069e8
DanDoge <30900990+DanDoge@users.noreply.github.com>
2018-05-22 17:43:05 +0800

    update: 05-22

--- view selection/src/surface_new/main.cpp
+++ view selection/src/surface_new/main.cpp
@@ -19,24 +19,22 @@
 #include <algorithm>
 
 
-void calc_distinctness(char str[], std::vector<double>& distinctness, std::vector<std::set<int> >& adj_table, std::vector<std::vector<double> >& dist){
+void calc_distinctness(obj::ObjModel& m, std::vector<double>& distinctness, std::vector<std::set<int> >& adj_table, std::vector<std::vector<double> >& dist){
 
+    std::cout << "calc_distinctness start" << std::endl;
 // load mesh
-    std::ifstream in(str);
-    obj::ObjModel m = obj::parseObjModel(in);
 
     std::vector<double> edge_length;
     std::vector<std::vector<double> > spin_image;
-
-    distinctness.reserve(m.vertex.size() / 3);
-    spin_image.reserve(m.vertex.size() / 3);
+    spin_image.resize(m.vertex.size() / 3);
     for(int i = 0; i < spin_image.size(); i += 1){
-        spin_image[i].reserve(16 * 16);
+        spin_image[i].resize(16 * 16);
     }
-    m.normal.reserve(m.vertex.size());
+    std::cout << "finished" << std::endl;
 
 // get adj_table, edge_length, vertex normals
 
+    std::cout << "calc adj_table, edge_length, vertex normals...";
     for(std::map<std::string, obj::ObjModel::FaceList>::iterator g = m.faces.begin(); g != m.faces.end(); ++g){
         obj::ObjModel::FaceList& fl = g->second;
         int face_size = fl.first.size() / 3;
@@ -44,6 +42,7 @@ void calc_distinctness(char str[], std::vector<double>& distinctness, std::vecto
             int v1_idx = fl.first[i * 3 + 0].v;
             int v2_idx = fl.first[i * 3 + 1].v;
             int v3_idx = fl.first[i * 3 + 2].v;
+
 
             // update adj_table
             adj_table[v1_idx].insert(v2_idx); adj_table[v1_idx].insert(v3_idx);
@@ -60,6 +59,7 @@ void calc_distinctness(char str[], std::vector<double>& distinctness, std::vecto
                 v3[i] = m.vertex[v3_idx * 3 + i];
             }
 
+
             // get edge_length
             double length_12 = (v1[0] - v2[0]) * (v1[0] - v2[0]) + (v1[1] - v2[1]) * (v1[1] - v2[1]) + (v1[0] - v2[0]) * (v1[2] - v2[2]);
             double length_23 = (v2[0] - v3[0]) * (v2[0] - v3[0]) + (v2[1] - v3[1]) * (v2[1] - v3[1]) + (v2[0] - v3[0]) * (v2[2] - v3[2]);
@@ -67,6 +67,7 @@ void calc_distinctness(char str[], std::vector<double>& distinctness, std::vecto
             edge_length.push_back(length_12);
             edge_length.push_back(length_23);
             edge_length.push_back(length_31);
+
 
             // get vertex normals
             double face_normal[3] = {};
@@ -76,6 +77,8 @@ void calc_distinctness(char str[], std::vector<double>& distinctness, std::vecto
             }
             calc_cross_product(face_normal, v2, v3);
             normalize_3(face_normal);
+
+
             for(int i = 0; i < 3; i += 1){
                 m.normal[v1_idx * 3 + i] += face_normal[i];
                 m.normal[v2_idx * 3 + i] += face_normal[i];
@@ -84,6 +87,7 @@ void calc_distinctness(char str[], std::vector<double>& distinctness, std::vecto
         }
     }
 
+    std::cout << "and normalize them...";
     // normalize vertex normals
     for(int i = 0; i < m.normal.size(); i += 3){
         double tmp_normal[3] = {m.normal[i], m.normal[i + 1], m.normal[i + 2]};
@@ -100,7 +104,7 @@ void calc_distinctness(char str[], std::vector<double>& distinctness, std::vecto
 
 // calc spin images, O(n)
 
-    std::cout << "calc spin images start..." << std::endl;
+    std::cout << "calc spin images..." << std::endl;
     int idx_i = 0;
     for(std::vector<double>::iterator i = m.vertex.begin(); i != m.vertex.end(); i += 3, idx_i += 1){
         double normal_i[3] = {m.normal[idx_i * 3], m.normal[idx_i * 3 + 1], m.normal[idx_i * 3 + 2]};
@@ -130,7 +134,7 @@ void calc_distinctness(char str[], std::vector<double>& distinctness, std::vecto
     // write to file, temp code, must be removed
     std::cout << "write to temp file, prepare to call geo_dist..." << std::endl;
     std::ofstream out;
-    out.open("C:\\spider\\tmp\\geodesics_lib\\bin\\maxplanck\\maxplanck.obj", std::ios::out | std::ios::trunc);
+    out.open("C:\\spider\\tmp\\geodesics_lib\\bin\\eight.obj", std::ios::out | std::ios::trunc);
     //out << m.vertex.size() / 3 << ' ' << m.faces.begin()->second.first.size() / 3 << std::endl;
     for(int i = 0; i < m.vertex.size(); i += 3){
         out  << "v " << m.vertex[i] << ' ' << m.vertex[i + 1] << ' ' << m.vertex[i + 2] << std::endl;
@@ -149,19 +153,22 @@ void calc_distinctness(char str[], std::vector<double>& distinctness, std::vecto
     dist.resize(m.vertex.size() / 3);
 
     // yes, write it and read it, that is close source software!
-    char* cmd = "C:\\spider\\tmp\\geodesics_lib\\bin\\GeoTest.exe C:\\spider\\tmp\\geodesics_lib\\bin\\maxplanck\\maxplanck.obj -d -s ";
+    char* cmd = "C:\\spider\\tmp\\geodesics_lib\\bin\\GeoTest.exe C:\\spider\\tmp\\geodesics_lib\\bin\\eight.obj -a 0.5 -d -s ";
     for(int i = 0; i < m.vertex.size() / 3; i += 1){
+    	if(i % 1 == 0){
+    		std::cout << i << std::endl;
+		}
         char cmd_now[1024] = {};
         sprintf(cmd_now, "%s%d\n", cmd, i);
         printf("%s\n", cmd_now);
         system(cmd_now);
 
         std::ifstream in;
-        sprintf(cmd_now, "%s%d%s", "C:\\spider\\tmp\\geodesics_lib\\bin\\maxplanck\\maxplanck.obj.", i, ".dist");
+        sprintf(cmd_now, "%s%d%s", "C:\\spider\\tmp\\geodesics_lib\\bin\\eight.obj.", i, ".dist");
         in.open(cmd_now, std::ios::in);
 
         std::vector<double> distinct;
-        distinct.reserve(m.vertex.size() / 3);
+        distinct.resize(m.vertex.size() / 3);
         dist[i].resize(m.vertex.size() / 3);
 
 // calc distinctness here! in this loop
@@ -171,7 +178,7 @@ void calc_distinctness(char str[], std::vector<double>& distinctness, std::vecto
             distinct[j] = calc_diffusion_dist(spin_image[i], spin_image[j]);
             distinct[j] /= 1 + 3 * dist[i][j];
         }
-        std::sort(dist.begin(), dist.begin() + m.vertex.size() / 3);
+        std::sort(dist[i].begin(), dist[i].begin() + m.vertex.size() / 3);
         int K = m.vertex.size() / 60;
         double res = 0.0f;
         for(int j = 0; j < K; j += 1){
@@ -180,6 +187,7 @@ void calc_distinctness(char str[], std::vector<double>& distinctness, std::vecto
         res = 1 - exp(-res / K);
         distinctness[i] = res;
     }
+
     return ;
 }
 
@@ -216,6 +224,7 @@ void calc_extremities(std::vector<std::vector<double> > &dist, std::vector<std::
     std::vector<std::vector<double> > u;
     std::vector<double> s;
     std::vector<std::vector<double> > v;
+
     svd(dist, 3, u, s, v);
     for(int i = 0; i < s.size(); i += 1){
         std::cout << s[i] << ' ';
@@ -246,7 +255,7 @@ void calc_extremities(std::vector<std::vector<double> > &dist, std::vector<std::
 // get extreme points
 
     std::vector<double> sum_of_geo_dist;
-	sum_of_geo_dist.reserve(dist.size());
+	sum_of_geo_dist.resize(dist.size());
     for(int i = 0; i < sum_of_geo_dist.size(); i += 1){
         for(int j = 0; j < sum_of_geo_dist.size(); j += 1){
             sum_of_geo_dist[i] += dist[i][j];
@@ -275,7 +284,15 @@ void calc_extremities(std::vector<std::vector<double> > &dist, std::vector<std::
 }
 
 void patch_association(std::vector<double> &distinctness, std::vector<int> &extremity_points, std::vector<std::vector<double> > &dist){
+    for(int i = 0; i < distinctness.size(); i += 1){
+    	std::cout << i << " " << distinctness[i] << std::endl;
+	}
+
     std::vector<double> tmp_for_sort = distinctness;
+
+    for(int i = 0; i < distinctness.size(); i += 1){
+    	std::cout << i << " " << distinctness[i] << std::endl;
+	}
     sort(tmp_for_sort.begin(), tmp_for_sort.end());
     double focus_threhold = tmp_for_sort[tmp_for_sort.size() * 4 / 5];
     // we treat it as a bool array from now on
@@ -298,7 +315,10 @@ void patch_association(std::vector<double> &distinctness, std::vector<int> &extr
             }
         }
         dist_a[i] = min_d_foci * exp(- min_geod * min_geod * 200);
+        std::cout << dist_a[i] << " ";
     }
+    std::cout << std::endl;
+
 
     std::vector<double> dist_e(distinctness.size());
     for(int i = 0; i < distinctness.size(); i += 1){
@@ -310,7 +330,10 @@ void patch_association(std::vector<double> &distinctness, std::vector<int> &extr
                 min_d_ext = distinctness[extremity_points[j]];
             }
         }
+        dist_e[i] = min_d_ext * exp(- min_geod * min_geod * 200);
+        std::cout << dist_e[i] << " ";
     }
+    std::cout << std::endl;
 
     for(int i = 0; i < distinctness.size(); i += 1){
         distinctness[i] = std::max((distinctness[i] + dist_a[i]) / 2, dist_e[i]);
@@ -318,7 +341,8 @@ void patch_association(std::vector<double> &distinctness, std::vector<int> &extr
 }
 
 int main(){
-
+	std::ios::sync_with_stdio(false);
+/*
     char str_4[] = "test_4.obj";
     std::vector<double> distinctness_4;
     std::vector<std::set<int> > adj_table_4;
@@ -332,18 +356,31 @@ int main(){
     calc_distinctness(str_2, distinctness_2, adj_table_2, dist_2);
 
     map_back(str_4, str_2, distinctness_4, distinctness_2);
-
+*/
     char str[] = "test.obj";
+    std::cout << "loading mesh " << str << "...";
+    std::ifstream in(str);
+    obj::ObjModel m = obj::parseObjModel(in);
+    m.normal.resize(m.vertex.size());
+
     std::vector<double> distinctness;
     std::vector<std::set<int> > adj_table;
     std::vector<std::vector<double> > dist;
-    calc_distinctness(str, distinctness, adj_table, dist);
+    distinctness.resize(m.vertex.size() / 3);
+    adj_table.resize(m.vertex.size() / 3);
 
-    map_back(str_2, str, distinctness_2, distinctness);
+    calc_distinctness(m, distinctness, adj_table, dist);
+
+//    map_back(str_2, str, distinctness_2, distinctness);
 
     std::vector<int> extremity_points;
     calc_extremities(dist, adj_table, extremity_points);
 
     patch_association(distinctness, extremity_points, dist);
+
+    std::cout << "results are..." << std::endl;
+    for(int i = 0; i < m.vertex.size() / 3; i += 1){
+    	std::cout << i << " " << distinctness[i] << std::endl;
+	}
     return 0;
 }
